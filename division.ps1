66620b2bb6e9e9995d3d9c5e23400dada014b6a7
grk0519 <66769519+grk0519@users.noreply.github.com>
2021-09-18 12:02:29 +0530

    Add files via upload
    simple Division PowerShell script

---
+++ division.ps1
@@ -0,0 +1,20 @@
+﻿#simple Division Powershell Script
+
+[int]$divident = Read-Host -Prompt "Enter Divident Number"
+[int]$divisor = Read-Host -Prompt "Enter Divisor"
+
+$reminder = $divident % $divisor
+
+Write-Host "=======================================`n"
+Write-Host $divident "/" $divisor ":"
+if($reminder -ne 0){
+    [int]$quotient = [math]::Floor($divident / $divisor)
+    
+    Write-Host "Quotient is :" $quotient
+    Write-Host "Reminder is: " $reminder
+}
+else{
+    $quotient = $divident / $divisor
+    Write-Host "Quotient is: " $quotient
+    Write-Host "Reminder is: " $reminder 
+}
