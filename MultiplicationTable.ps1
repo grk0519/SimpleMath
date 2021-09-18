109855add480658178b6e9e87cf4d2e76fbef8cf
grk0519 <66769519+grk0519@users.noreply.github.com>
2021-09-18 11:52:42 +0530

    Add files via upload
    Simple PowerShell script to print the multiplication table of a given number

---
+++ MultiplicationTable.ps1
@@ -0,0 +1,7 @@
+﻿#simple Multiplication Table Powershell Script
+$range = 1..12
+[int]$num = Read-Host -Prompt "Enter a Number only"
+Write-Host "`n========= $($num) Multiplication Table ==========`n"
+foreach($r in $range){
+    Write-Host $num "x" $r "=" $($num * $r)
+}
